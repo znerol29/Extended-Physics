e27e1bc7e5bd51e029f71b1e064833ba8c8614d8
Lorenz <160867235+znerol29@users.noreply.github.com>
2024-02-23 10:17:02 +0100

    Change like everything

--- extendedphysics.py
+++ extendedphysics.py
@@ -1,6 +1,3 @@
-# Set title for filedialog and add instructions
-# Finish def restore(), Maybe use restore(Variable)
-
 import os
 import sys
 import tkinter as tk
@@ -24,25 +21,26 @@ else:
         acpath = f.read().strip()
   else:
     # Ask for AC directory
-    acpath = filedialog.askdirectory()
+    acpath = filedialog.askdirectory(title = "Select Assetto Corsa directory (steamapps/common/assettocorsa)")
     if acpath:
       with open("acpath.txt", "w") as f:
         f.write(acpath)
   # Open filedialog in AC directory and ask for track path
-  directory = filedialog.askdirectory(initialdir=acpath)
+  directory = filedialog.askdirectory(initialdir=acpath, title = "Select AC track folder (assettocorsa/content/tracks/Name of the Track)")
 
 os.chdir(directory)
 
+layoutcount = 0
 restorecount = 0
 backupcount = 0
 nobackup = 0
 errorcount = 0
 
 def rename():
-  if os.path.isfile("surfaces - Kopie.ini")
-  os.rename("surfaces - Kopie.ini", "surfaces-backup.ini")
+  if os.path.isfile("surfaces - Kopie.ini"):
+    os.rename("surfaces - Kopie.ini", "surfaces-backup.ini")
 
-def restore():
+def restore(workdir):
   if os.path.isfile("surfaces.ini"):
     with open("surfaces.ini", "r") as f:
       if "extended" in f.read():
@@ -52,61 +50,33 @@ def restore():
           with open("surfaces.ini") as f3:
             f3.write(contents)
           restorecount += 1
+        else:
+          nobackup += 1
       else:
         if not os.path.isfile("surfaces-backup.ini"):
           try:
-            shutil.copy(directory
+            shutil.copy(workdir + "/data/surfaces.ini", workdir + "/data/surfaces-backup.ini")
+            backupcount += 1
+          except:
+            errorcount += 1
 
+
+# If only one layout
 if os.path.isdir("data"):
-    os.chdir(directory + "\data")
-# If only one layout for track (Dir "\data" in top folder)
-    if os.path.isfile("surfaces.ini"):
-        with open("surfaces.ini", "r") as f:
-            if "extended" in f.read():
-              rename()
-                if os.path.isfile("surfaces - Kopie.ini"):
-                    with open("surfaces - Kopie.ini", "r") as f2:
-                        contents = f2.read()
-                    with open("surfaces.ini", "w") as f3:
-                        f3.write(contents)
-                    print("Success\n\n" + "'surfaces.ini' has been restored!")
+  os.chdir(directory + "/data")
+  restore(directory + "/data")
+  layoutcount += 1
 
-                else:
-                    print("Failure\n\n" + "No backup was found.")
-            else:
-                if not os.path.isfile("surfaces - Kopie.ini"):
-                    try:
-                        shutil.copy(directory + "/data/surfaces.ini", directory + "/data/surfaces - Kopie.ini")
-                        print("Success\n\n" + "A backup of 'surfaces.ini' has been created")
-                    except:
-                        print("Failure\n\n" + "An error occured while backing up 'surfaces.ini'")
-    else:
-        print("Failure\n\n" + "No file named 'surfaces.ini' was not found.")
-
+# If multiple layouts
 else:
-  # Else restore for all layouts
+  for root, dirs, files in os.walk(directory):
+    if "surfaces.ini" in files:
+      os.chdir(root)
+      restore(root)
+      layoutcount += 1
+    os.chdir(directory)
 
-    for root, dirs, files in os.walk(directory):
-        if "surfaces.ini" in files:
-            os.chdir(root)
-            with open("surfaces.ini", "r") as f:
-                if "extended" in f.read():
-                    if os.path.isfile("surfaces - Kopie.ini"):
-                        with open("surfaces - Kopie.ini", "r") as f2:
-                            contents = f2.read()
-                        with open("surfaces.ini", "w") as f3:
-                            f3.write(contents)
-                        restorecount += 1
-                    else:
-                        nobackup += 1
-                else:
-                    if not os.path.isfile("surfaces - Kopie.ini"):
-                        try:
-                            shutil.copy(root + "/surfaces.ini", root + "/surfaces - Kopie.ini")
-                            backupcount += 1
-                        except:
-                            errorcount += 1
-        os.chdir(directory)
-    print("Finished\n\n" + str(restorecount) + " files were restored.\n" + str(backupcount) + " files were backed up.\n" + str(nobackup) + " files had no backup.\n" + str(errorcount) + " errors occured.")
+# Show results and exit after 2s
+print("Finished\n\n" + str(layoutcount) + " layouts were found.\n" + str(restorecount) + " files were restored.\n" + str(backupcount) + " files were backed up.\n" + str(nobackup) + " files had no backup.\n" + str(errorcount) + " errors occured.")
 time.sleep(2)
 exit(0)
